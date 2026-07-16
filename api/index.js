27d5208d19550a0ae7c6db0f4fd01565bd46a760
Dhruba Majumder <60184931+DHRUBA-MAJUMDER@users.noreply.github.com>
2026-07-16 16:26:59 +0530

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -18,6 +18,11 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("REQUEST:", req.method, req.originalUrl);
+  next();
+});
+
 // --- CONFIGURATIONS ---
 
 // 1. Razorpay Setup
